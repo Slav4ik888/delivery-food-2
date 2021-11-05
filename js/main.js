20cc6a045a02db23ad3c5cdc2f49aa9d3973c012
Vyacheslav <korzan.va@mail.ru>
2021-11-05 11:59:31 +0500

    1 lesson 2 part

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const logInForm      = document.getElementById(`logInForm`);
 const inputLogin     = document.getElementById(`login`);
 const inputPassword  = document.getElementById(`password`);
 const userName       = document.querySelector(`.user-name`);
+const errorLogin     = document.querySelector(`.error-login`);
+const errorPassword  = document.querySelector(`.error-password`);
+
 
 const savedUser = localStorage.getItem(`user`) ? JSON.parse(localStorage.getItem(`user`)) : null;
 
@@ -30,12 +33,32 @@ function authSubmit(e) {
     login: inputLogin.value,
     password: inputPassword.value
   };
+  
+  const valid = validationForm(user);
+  if (!valid) return;
+
   localStorage.setItem(`user`, JSON.stringify(user));
   login(user);
   toggleModal();
 }
 
+function validationForm(user) {
+  if (!user.login) {
+    errorLogin.textContent = `Поле "Логин" должно быть заполнено`;
+  }
+  if (!user.password) {
+    errorPassword.textContent = `Поле "Пароль" должно быть заполнено`;
+  }
+  return user.login && user.password
+}
+
+function clearError() {
+  errorLogin.textContent = ``;
+  errorPassword.textContent = ``;
+}
+
 function login(user) {
+  clearError();
   console.log('user: ', user);
   btnAuth.style.display = `none`;
   btnOut.style.display = `flex`;
@@ -54,3 +77,6 @@ function logout() {
 if (savedUser) {
   login(savedUser);
 }
+
+
+// git add . && git commit -m "1 lesson 2 part" && git push -u origin master
